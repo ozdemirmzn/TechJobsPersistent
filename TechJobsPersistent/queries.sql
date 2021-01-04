a02596079227616af124ccfa5621f2c539f14ce0
Ramazan Özdemir <ozdemirmzn@gmail.com>
2021-01-04 10:51:46 -0600

    done

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,6 +1,11 @@
 --Part 1
-
+SELECT * FROM techjobs.jobs;
 --Part 2
-
+SELECT name
+	FROM techjobs.employers
+	WHERE location = "St. Louis City";
 --Part 3
-
+SELECT DISTINCT Name, Description
+	FROM techjobs.skills
+	INNER JOIN techjobs.jobskills ON techjobs.skills.Id = techjobs.jobskills.SkillId
+	ORDER BY Name ASC;
